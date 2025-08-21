923564fc48d8032f9cf7568e6e8fbfe0913c44c1
NikiTosikM <marykov63@mail.ru>
2025-08-21 12:37:03 +0300

    paginashion

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query
 import uvicorn
 
 
@@ -7,12 +7,31 @@ app = FastAPI()
 
 hotels = [
     {"id": 1, "title": "Sochi", "name": "sochi"},
-    {"id": 2, "title": "Dubai", "name": "dubai"}
+    {"id": 2, "title": "Дубай", "name": "dubai"},
+    {"id": 3, "title": "Мальдивы", "name": "maldivi"},
+    {"id": 4, "title": "Геленджик", "name": "gelendzhik"},
+    {"id": 5, "title": "Москва", "name": "moscow"},
+    {"id": 6, "title": "Казань", "name": "kazan"},
+    {"id": 7, "title": "Санкт-Петербург", "name": "spb"}
 ]
 
-@app.get("/hotels/")
-def get_hotels():
-    return hotels
+@app.get("/hotels")
+def get_hotels(
+    id: int | None = Query(default=None, description='идентификатор отеля'),
+    title: str | None = Query(default=None, description="Название нашего отеля"),
+    page: int | None = Query(default=1, description="Номер страницы для отображения"),
+    per_page : int | None = Query(default=5, description="Количество отображаемых элементов на странице")
+):
+    hotels_ = []
+    for hotel in hotels:
+        if title and hotel["title"] != title:
+            continue
+        if id and hotel["id"] != id:
+            continue
+        hotels_.append(hotel)
+    start, end = (page-1) * per_page, page * per_page
+    return hotels_[start: end]
+            
 
 
 @app.put("/hotels/{hotel_id}")
@@ -34,11 +53,13 @@ def change_hotel(
     title: str | None = Body(default=None),
     name: str | None = Body(default=None),
 ):
-    hotels[hotel_id-1] = {
-        "id": hotel_id,
-        "title": title if title else hotels[hotel_id-1]["title"],
-        "name": name if name else hotels[hotel_id-1]["name"]
-    }
+    hotel = hotels[hotel_id-1]
+    if title:
+        hotel["title"] = title
+    elif name:
+        hotel["name"] = name
+    
+    return {"sucess": "ok"}
     
     
 
