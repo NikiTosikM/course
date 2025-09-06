32f8d3e1bf65f123eaf91354a5ad6158fcc9471a
NikiTosikM <marykov63@mail.ru>
2025-09-06 12:54:59 +0300

    filter by title location

--- src/core/db/base_model.py
+++ src/core/db/base_model.py
@@ -1,5 +1,15 @@
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from core.config import settings
 
 
 class Base(DeclarativeBase):
     pass
+
+
+async_engine = create_async_engine(url=settings.db.get_db_url, echo=True)
+
+async_session_maker: async_sessionmaker = async_sessionmaker(
+    async_engine, expire_on_commit=False
+)
--- src/main.py
+++ src/main.py
@@ -1,68 +1,58 @@
 from typing import Annotated
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(__file__).parent.parent))
 
 
-from fastapi import FastAPI, Body, Query
+from fastapi import FastAPI, Query
 import uvicorn
+from sqlalchemy import select, Result
+from models.hotels import Hotels
 
 from schemas.hotels import PiganHotelDep
+from core.db.base_model import async_session_maker
 
 
 app = FastAPI()
 
 
-hotels = [
-    {"id": 1, "title": "Sochi", "name": "sochi"},
-    {"id": 2, "title": "Дубай", "name": "dubai"},
-    {"id": 3, "title": "Мальдивы", "name": "maldivi"},
-    {"id": 4, "title": "Геленджик", "name": "gelendzhik"},
-    {"id": 5, "title": "Москва", "name": "moscow"},
-    {"id": 6, "title": "Казань", "name": "kazan"},
-    {"id": 7, "title": "Санкт-Петербург", "name": "spb"},
-]
-
-
 @app.get("/hotels")
-def get_hotels(
+async def get_hotels(
     pig_hotels: PiganHotelDep,
-    id: Annotated[int | None, Query(description="идентификатор отеля")] = None,
+    location: Annotated[str | None, Query(description="город где находится отель")] = None,
     title: Annotated[str | None, Query(description="Название отеля")] = None,
 ):
-    hotels_ = []
-    for hotel in hotels:
-        if title and hotel["title"] != title:
-            continue
-        if id and hotel["id"] != id:
-            continue
-        hotels_.append(hotel)
-    start, end = (
-        (pig_hotels.page - 1) * pig_hotels.per_page,
-        pig_hotels.page * pig_hotels.per_page,
-    )
-    return hotels_[start:end]
-
-
-@app.put("/hotels/{hotel_id}")
-def update_hotel(hotel_id: int, title: str = Body(), name: str = Body()):
-    hotels[hotel_id - 1] = {"id": hotel_id, "title": title, "name": name}
-
-
-@app.patch("/hotels/{hotel_id}")
-def change_hotel(
-    hotel_id: int,
-    title: str | None = Body(default=None),
-    name: str | None = Body(default=None),
-):
-    hotel = hotels[hotel_id - 1]
+    query = select(Hotels)
     if title:
-        hotel["title"] = title
-    elif name:
-        hotel["name"] = name
+        query = query.where(Hotels.title.like(f"%{title}%"))
+    if location:
+        query = query.where(Hotels.location.like(f"%{location}%"))
+    query = (
+        query
+        .limit(pig_hotels.per_page)
+        .offset((pig_hotels.page-1) * pig_hotels.per_page)
+    )
+    async with async_session_maker() as session:
+        result: Result =  await session.execute(query)
+        
+    return result.scalars().all()
 
-    return {"sucess": "ok"}
+
+# @app.put("/hotels/{hotel_id}")
+# def update_hotel(hotel_id: int, title: str = Body(), name: str = Body()):
+#     hotels[hotel_id - 1] = {"id": hotel_id, "title": title, "name": name}
+
+
+# @app.patch("/hotels/{hotel_id}")
+# def change_hotel(
+#     hotel_id: int,
+#     title: str | None = Body(default=None),
+#     name: str | None = Body(default=None),
+# ):
+#     hotel = hotels[hotel_id - 1]
+#     if title:
+#         hotel["title"] = title
+#     elif name:
+#         hotel["name"] = name
+
+#     return {"sucess": "ok"}
 
 
 def main():
