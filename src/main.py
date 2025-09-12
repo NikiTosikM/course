9cb692893ce359eca67f267a7f04f82f99c6afb4
NikiTosikM <marykov63@mail.ru>
2025-09-12 20:40:37 +0400

    edit bugs in add method

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
 from typing import Annotated
 
-
-from fastapi import FastAPI, Query
 import uvicorn
-from models.hotels import Hotels
+from fastapi import FastAPI, Query
 
-from schemas.hotels import PiganHotelDep, HotelSchema
 from core.db.base_model import async_session_maker
+from models.hotels import Hotels
 from repositories.hotel_repository import HoterRepository
+from schemas.hotels import HotelSchema, PiganHotelDep, HotelResponceSchema
 
 
 app = FastAPI()
@@ -35,11 +34,14 @@ async def get_hotels(
 
 
 @app.post("/hotels")
-async def create_hotel(hotel_data: HotelSchema) -> str:
+async def create_hotel(hotel_data: HotelSchema) -> dict:
     async with async_session_maker() as session:
-        hotel: Hotels = await HoterRepository(session=session, model=Hotels).add(
+        hotel_model: Hotels = await HoterRepository(session=session, model=Hotels).add(
             data=hotel_data
         )
+        await session.commit()
+        
+    hotel = HotelResponceSchema.model_validate(hotel_model)
 
     return {"status": "OK", "data": hotel}
 
--- src/repositories/base_repository.py
+++ src/repositories/base_repository.py
@@ -28,7 +28,7 @@ class BaseRepository(Generic[Model]):
 
         return result.scalars().all()
 
-    async def get_one_or_none(self, **filter_by):
+    async def get_one_or_none(self, **filter_by) -> Model:
         query = select(self.model).filter_by(**filter_by)
         result: Result = await self.session.execute(query)
 
